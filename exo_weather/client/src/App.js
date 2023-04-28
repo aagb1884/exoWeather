2df31492cceae812e2b4450b3ef9113e94daa5f8
Andrew Blair <a.a.g.blair@gmail.com>
2023-04-28 21:03:13 +0100

    6th commit - sorting files, folders, doing first npm start on client side

--- exo_weather/client/src/App.js
+++ exo_weather/client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header_Nav from './Components/Header_Nav';
+import Container from './Containers/Container';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>App</h1>
+       <h1>exoWeather App</h1>
        <Header_Nav />
+       <Container />
       </header>
     </div>
   );

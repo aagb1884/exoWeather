00c41a2458d2275549b8a0913161c7d8e4240337
Andrew Blair <a.a.g.blair@gmail.com>
2023-05-01 14:11:43 +0100

    working on planet select function

--- exo_weather/client/src/Components/PlanetBox.js
+++ exo_weather/client/src/Components/PlanetBox.js
@@ -1,12 +1,31 @@
 import PlanetSelect from "./PlanetSelect";
 import UniquePlanet from "./UniquePlanet";
+import React, { useState, useEffect } from 'react';
 
 const PlanetBox = () => {
+    const [planets, setPlanets] = useState([]);
+    const [selectedPlanet, setSelectedPlanet] = useState(null);
+
+    useEffect(() => {
+        getPlanets();
+    }, [])
+
+    const getPlanets = function(){
+
+    }
+
+    const onPlanetSelect = function(planetName) {
+        const foundPlanet = planets.find((country) => planet.name === planetName )
+        setSelectedPlanet(foundPlanet)
+    }
+
+
+
     return ( 
         <>
          <h2>Planet Box</h2>
          <UniquePlanet />
-         <PlanetSelect />
+         <PlanetSelect planets={planets} onPlanetSelect={onPlanetSelect}/>
         </>
      );
 }
--- exo_weather/client/src/Components/PlanetSelect.js
+++ exo_weather/client/src/Components/PlanetSelect.js
@@ -1,7 +1,19 @@
-const PlanetSelect = () => {
-    return ( <>
-            <h3>Planet Selector</h3>
-            </> );
+const PlanetSelect = (planets, onPlanetSelect) => {
+
+    const handleChange = function(event) {
+        const chosenPlanetName = event.target.value;
+        onPlanetSelect(chosenPlanetName)
+    }
+
+    const planetOptions = planets.map((planet) => {
+        return <option value={planet.name}>{planet.name}</option>
+    })
+
+    return ( 
+            <select defaultValue="" onChange={handleChange}>
+                {planetOptions}
+            </select> 
+            )
 }
  
 export default PlanetSelect;
